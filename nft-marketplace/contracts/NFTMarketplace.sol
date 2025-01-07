032f0394431cb24c768bbc28d1a55be8b860d130
Rahul Sahani <yourahul1917@gmail.com>
2025-01-08 00:55:42 +0530

    Added Hardhat and NFTMarket smart contract

---
+++ nft-marketplace/contracts/NFTMarketplace.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+
+}
